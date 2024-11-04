d1514227584518a0bbb15b00d209016f963348a8
yuwnloy <yuwnloyblog@163.com>
2024-11-04 10:26:53 +0800

    add accept/hangup for rtc signal

--- services/rtcroom/starter.go
+++ services/rtcroom/starter.go
@@ -38,6 +38,12 @@ func (manager *RtcRoomManager) RegisterActors(register gmicro.IActorRegister) {
 	register.RegisterActor("rtc_invite", func() actorsystem.IUntypedActor {
 		return bases.BaseProcessActor(&actors.InviteActor{}, serviceName)
 	})
+	register.RegisterActor("rtc_accept", func() actorsystem.IUntypedActor {
+		return bases.BaseProcessActor(&actors.AcceptActor{}, serviceName)
+	})
+	register.RegisterActor("rtc_hangup", func() actorsystem.IUntypedActor {
+		return bases.BaseProcessActor(&actors.HangupActor{}, serviceName)
+	})
 	register.RegisterActor("rtc_member_rooms", func() actorsystem.IUntypedActor {
 		return bases.BaseProcessActor(&actors.QryRtcMemberRoomsActor{}, serviceName)
 	})
