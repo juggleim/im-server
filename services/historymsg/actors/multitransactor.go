6976f748c9df6e4af118b54e2822ef097e0ee58f
yuwnloy <yuwnloyblog@163.com>
2024-12-31 18:03:45 +0800

    add param check for trans api

--- services/historymsg/actors/multitransactor.go
+++ services/historymsg/actors/multitransactor.go
@@ -20,27 +20,31 @@ type MultiTransActor struct {
 func (actor *MultiTransActor) OnReceive(ctx context.Context, input proto.Message) {
 	if req, ok := input.(*pbobjs.TransReq); ok {
 		logs.WithContext(ctx).Infof("user_id:%s\tsource_lang:%s\ttarget_lang:%s\tlen:%d", bases.GetRequesterIdFromCtx(ctx), req.SourceLang, req.TargetLang, len(req.Items))
-		transEngine := commonservices.GetTransEngine(bases.GetAppKeyFromCtx(ctx))
 		resp := &pbobjs.TransReq{
 			Items:      []*pbobjs.TransItem{},
 			SourceLang: req.SourceLang,
 			TargetLang: req.TargetLang,
 		}
 		code := errs.IMErrorCode_SUCCESS
-		if transEngine != nil && transEngine != transengines.DefaultTransEngine {
-			for _, item := range req.Items {
-				result := transEngine.Translate(item.Content, []string{req.TargetLang})
-				if len(result) > 0 {
-					if afterTranslated, exist := result[req.TargetLang]; exist {
-						resp.Items = append(resp.Items, &pbobjs.TransItem{
-							Key:     item.Key,
-							Content: afterTranslated,
-						})
+		if req.TargetLang == "" || len(req.Items) <= 0 {
+			code = errs.IMErrorCode_CONNECT_PARAM_REQUIRED
+		} else {
+			transEngine := commonservices.GetTransEngine(bases.GetAppKeyFromCtx(ctx))
+			if transEngine != nil && transEngine != transengines.DefaultTransEngine {
+				for _, item := range req.Items {
+					result := transEngine.Translate(item.Content, []string{req.TargetLang})
+					if len(result) > 0 {
+						if afterTranslated, exist := result[req.TargetLang]; exist {
+							resp.Items = append(resp.Items, &pbobjs.TransItem{
+								Key:     item.Key,
+								Content: afterTranslated,
+							})
+						}
 					}
 				}
+			} else {
+				code = errs.IMErrorCode_CONNECT_UNSUPPORTEDTOPIC
 			}
-		} else {
-			code = errs.IMErrorCode_CONNECT_UNSUPPORTEDTOPIC
 		}
 		ack := bases.CreateQueryAckWraper(ctx, code, resp)
 		actor.Sender.Tell(ack, actorsystem.NoSender)
