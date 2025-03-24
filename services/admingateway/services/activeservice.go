e426924be5175151fad65e550f875347af80c8a2
litaotxws <litaotxws@163.com>
2025-03-24 14:59:18 +0800

    fix: appkey

--- services/admingateway/services/activeservice.go
+++ services/admingateway/services/activeservice.go
@@ -1,13 +1,21 @@
 package services
 
 import (
+	"fmt"
 	"im-server/commons/tools"
 	"im-server/services/commonservices/dbs"
+	"math/rand"
 	"time"
 )
 
 func CreateApp(appInfo AppInfo) (AdminErrorCode, *AppInfo) {
 	dao := dbs.AppInfoDao{}
+	if len(appInfo.AppKey) != 18 {
+		key := tools.RandStr(18)
+		rand.New(rand.NewSource(time.Now().UnixNano()))
+		randomNum := rand.Intn(10000)
+		appInfo.AppKey = fmt.Sprintf("%s%04d", key, randomNum)
+	}
 	dbAppInfo := dao.FindByAppkey(appInfo.AppKey)
 	if dbAppInfo != nil && dbAppInfo.AppKey == appInfo.AppKey {
 		return AdminErrorCode_AppHasExisted, &AppInfo{
